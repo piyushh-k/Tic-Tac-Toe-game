310cfa1eb17742394d2a2cba774708974ad670bb
piyush <piyushkochar909@gmail.com>
2025-02-02 23:17:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,6 @@
 import Game from "./square";
 
 function App() {
-  const array1 = [18 , 12 , 10]
-  function check(value){
-    return value==null;
-  }
-
-  const checked = array1.filter(check)
-  console.log(checked)
-  if(checked.length == []){
-    console.log("empty")
-  }
-
   return (
     <>
       <center>
