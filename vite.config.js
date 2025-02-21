b20eda2e4aace5084882ed5ab98a747ffaf36ace
piyush <piyushkochar909@gmail.com>
2025-02-21 15:18:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Tic-Tac-Toe-game/', // Set this to match your GitHub repository path
   server: {
     allowedHosts: [
       '3558-122-161-65-199.ngrok-free.app'
     ]
   }
 })
+
