89e16c66d542b845469b7f2b41ee3aee62a196e8
piyush <piyushkochar909@gmail.com>
2025-02-22 16:32:14 +0530

    added build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Tic-Tac-Toe-game/', // Set this to match your GitHub repository path
+  base: '/Tic-Tac-Toe-game/',
+  build: {
+    outDir: 'dist',
+  },
   server: {
     allowedHosts: [
       '3558-122-161-65-199.ngrok-free.app'
